075371c91daee175b8fa98789f1765a376fb9e09
c.fortezza2000 <c.fortezza@campus.unimib.it>
2024-01-13 19:20:15 +0100

    Solved HLD in UserDTO

---
+++ src/main/java/io/github/jhipster/online/service/dto/UserDTOHLD.java
@@ -0,0 +1,12 @@
+package io.github.jhipster.online.service.dto;
+
+import io.github.jhipster.online.domain.User;
+
+public class UserDTOHLD extends UserDTO {
+
+    public UserDTOHLD() {}
+
+    public UserDTOHLD(User user) {
+        super(user);
+    }
+}
